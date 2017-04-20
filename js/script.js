f1ca9331bc9896d08c1bd4fa79e381075ef7ab43
sorefull <olegh.cherednichenko@gmail.com>
2017-04-21 00:47:49 +0300

    slider, navs

--- js/script.js
+++ js/script.js
@@ -1,8 +1,36 @@
 $(document).ready(function() {
-  $('.items div').first().show();
+  $('.item').first().show();
   $('.sqare').on('click', function(){
     var index = $('.sqare').index(this);
-    $('.items div').hide();
-    $('.items div').eq(index).fadeIn();
+    $('.sqare.active').removeClass('active');
+    $(this).addClass('active');
+    $('.item').hide();
+    $('.item').eq(index).fadeIn();
+  });
+  $('.side-left').hover(function() {
+    $('.glyphicon-menu-left').css('color', '#fff');
+  }, function() {
+    $('.glyphicon-menu-left').css('color', '#130000');
+  });
+  $('.side-right').hover(function() {
+    $('.glyphicon-menu-right').css('color', '#fff');
+  }, function() {
+    $('.glyphicon-menu-right').css('color', '#130000');
+  });
+  $('.side-left').on('click', function() {
+    var item_index = $('.item').index($(this).parent().parent());
+    if ($('.sqare').eq(item_index - 1).length == 0) {
+      $('.sqare').last().click();
+    } else {
+      $('.sqare').eq(item_index - 1).click();
+    }
+  });
+  $('.side-right').on('click', function() {
+    var item_index = $('.item').index($(this).parent().parent());
+    if ($('.sqare').eq(item_index + 1).length == 0) {
+      $('.sqare').eq(0).click();
+    } else {
+      $('.sqare').eq(item_index + 1).click();
+    }
   });
 });
